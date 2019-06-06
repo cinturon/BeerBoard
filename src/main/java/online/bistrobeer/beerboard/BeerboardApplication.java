f5f008bc572611e16c03f8a35299b1057dbe1d04
jeremybelt <jeremybelt@gmail.com>
2019-06-05 20:43:06 -0700

    Initial commit

---
+++ src/main/java/online/bistrobeer/beerboard/BeerboardApplication.java
@@ -0,0 +1,13 @@
+package online.bistrobeer.beerboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerboardApplication.class, args);
+    }
+
+}
